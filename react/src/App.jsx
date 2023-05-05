dfde3017754bfba0c1863de2b0191093dffe80b5
siusiusiublack1 <siusiusiublack@gmail.com>
2023-05-05 17:29:35 +0800

    20230505FirstTestTailWind

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import "./index.css"
-
+import { BeakerIcon } from '@heroicons/react/24/solid'
 
 function App() {
 
@@ -9,6 +9,7 @@ function App() {
 
     <div className="bg-purple-500">
       App.jsx
+      <BeakerIcon className="w-4" />
     </div>
   )
 }
