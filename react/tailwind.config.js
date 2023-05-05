162467e03e844139e2a389fc6cb8f6d44baa851e
siusiusiublack1 <siusiusiublack@gmail.com>
2023-05-05 17:18:37 +0800

    succeedTailwindCss

---
+++ react/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './src/pages/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+    './src/containers/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
